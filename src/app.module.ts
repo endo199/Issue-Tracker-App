ffb75e32e45cd4c894e56efc80e4fa5b57d2e6f6
Suhendro <suhendro.mail@gmail.com>
2019-05-18 22:53:00 +0700

    - remove default scafolding apps - add database service to handle data - add issue module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { IssueModule } from './issue/issue.module';
+import { DatabaseService } from './database.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [IssueModule],
+  controllers: [],
+  providers: [DatabaseService],
 })
 export class AppModule {}
